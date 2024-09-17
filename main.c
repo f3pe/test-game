77192765e2e6d013c0e252bba46c9320adfd9ed4
f3pe <fel234bf4@gmail.com>
2024-09-17 16:11:21 -0300

    corrigindo erros autorais do código

--- main.c
+++ main.c
@@ -7,19 +7,17 @@
 float px, py, pdx, pdy, pa; //player possition
 
 /* Map config*/
-int mapX = 10, mapY=10, mapS = 60;
-int map[10][10] =
+int mapX = 8, mapY=8, mapS = 64;
+int map[] =
 {
-    {1,1,1,1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,0,1},
-    {1,1,1,1,1,1,1,1,1,1}
+    1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,
 };
 
 void setDeltaPlayer(){
@@ -32,6 +30,53 @@ void init() {
     px = 300; py = 300; pa = 0; 
     setDeltaPlayer();
 }
+
+void drawRays3D(){
+    int r, mx, my, mp, dof; 
+    float rx, ry, ra, xo, yo;
+    ra=pa;
+    for(r = 0; r < 1 ; r++){
+        dof=0;
+        float aTan = -1/tan(ra);
+        if(ra > PI){
+            ry = (((int)py>>6)<<6) - 0.0001;
+            rx = (py-ry) * aTan+px;
+            yo = -64;
+            xo = -yo*aTan;
+        }
+        if(ra < PI){
+            ry = (((int)py>>6)<<6) + 64;
+            rx = (py-ry) * aTan+px;
+            yo = 64;
+            xo = -yo*aTan;
+        }
+        if(ra == 0 || ra == PI){
+            rx = px;
+            ry = py;
+            dof = 8;
+        }
+        while(dof < 8){
+            mx=(int)(rx) >> 6;
+            my=(int)(ry) >> 6;
+            mp = my * mapX+mx;
+            if((mp < mapX * mapY) && (map[mp] == 1)){//hit wall
+                dof = 8;
+            }
+            else{
+                rx += xo;
+                ry += yo;
+                dof++;
+            }
+        }
+        glColor3f(.6, .6, .6);
+        glLineWidth(2);
+        glBegin(GL_LINES);
+        glVertex2i(px, py);
+        glVertex2i(rx, ry);
+        glEnd();
+    }
+}
+
 
 void drawPlayer(){
     glColor3f(1, 0, 0);
@@ -51,7 +96,7 @@ void drawMap2D(){
     int x, y, xo, yo;
     for(y = 0; y < mapY; y++){
         for(x = 0; x < mapX; x++){
-            if(map[y][x] == 1){ 
+            if(map[y*mapX+x] == 1){ 
                 glColor3f(0, 0, 0.75);
             }else{ 
                 glColor3f(0, 0, 0);
@@ -71,6 +116,7 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     drawMap2D();
     drawPlayer();
+    drawRays3D();
     glutSwapBuffers();
 }
 
